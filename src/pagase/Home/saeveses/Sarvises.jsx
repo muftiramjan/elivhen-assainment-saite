670712950ba6438788f35be2470fb9a75c78da98
mufti ramjan <alibinalaluddin@gmail.com>
2024-05-13 21:00:27 +0600

    faramer animetion complit

--- src/pagase/Home/saeveses/Sarvises.jsx
+++ src/pagase/Home/saeveses/Sarvises.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import SarvisesCard from "./SarvisesCard";
-
+import {motion,useScroll} from "framer-motion"
 
 
 const Sarvises = () => {
+    const {scrollYProgress}=useScroll();
     const [cards, setCard] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/carServes')
@@ -12,13 +13,28 @@ const Sarvises = () => {
 
     }, [cards])
     return (
-        <div className="text-center p-5 mt-10">
-            <h1 className="text-5xl font-bold">Food A Reflection of Culture</h1>
+        <>
+        <motion.div
+        style={{
+            scaleX:scrollYProgress,
+            position:  'inherit',
+            top:0,
+            left:0,
+            right:0,
+            height:0,
+            background:'gray',
+            transformOrigin:'%0'
+        }}
+        className="text-center p-1 mt-10">
+        </motion.div>
+          <div className="text-center p-5 mt-10">
+          <h1 className="text-5xl font-bold">Food A Reflection of Culture</h1>
             <p>Food is not just sustenance; it's a profound force that inspires humanity.
                  It embodies culture, connecting us to a rich tapestry of experiences, literature, 
                  <br />traditions, and societal norms. From its diverse forms to its flavors, health 
                 benefits, transmission, and its relationship with the environment, food seamlessly 
                  <br /> integrates into every aspect of our lives, serving as an essential component of our existence. </p>
+          </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 
                 {
@@ -30,7 +46,7 @@ const Sarvises = () => {
                 }
             </div>
           
-        </div>
+            </>
     );
 };
 
